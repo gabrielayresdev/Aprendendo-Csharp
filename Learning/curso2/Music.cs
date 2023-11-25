6cd30f5815d2825beea11d8bf6fe79fc595b5885
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-11-25 01:40:46 -0300

    curso 2: aplicando a orientação a objetos

--- Learning/curso2/Music.cs
+++ Learning/curso2/Music.cs
@@ -8,19 +8,30 @@ namespace Learning.curso2
 {
     internal class Music
     {
-        string name;
-        string artist;
-        string album;
-        string duration;
-        bool available;
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public string Album {  get; set; }
+        public string Duration { get; set; }
+        public bool Available { get; set; }
+        public string Description { 
+            get
+            {
+                return $"A música {Name}, duração {Duration} e pertencente ao album {Album} é cantada por {Artist}.";
+            }
+        }
+
+
+       
+
+
 
         public void printMusicData()
         {
-            Console.WriteLine($"Nome: {name}");
-            Console.WriteLine($"Artista: {artist}");
-            Console.WriteLine($"album: {album}");
-            Console.WriteLine($"duração: {duration}s");
-            if(available)
+            Console.WriteLine($"Nome: {Name}");
+            Console.WriteLine($"Artista: {Artist}");
+            Console.WriteLine($"album: {Album}");
+            Console.WriteLine($"duração: {Duration}s");
+            if(Available)
             {
                 Console.WriteLine("Disponível no plano");
             } else
