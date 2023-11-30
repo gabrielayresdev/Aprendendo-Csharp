c6a798f8c654f82e948f3a22a510db305a46f96a
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-11-30 19:51:01 -0300

    curso 2: aplicando a Orientação a Objetos

---
+++ Learning/curso2/Episodes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning.curso2
+{
+    internal class Episodes
+    {
+        public int Duration { get; set; }
+        public int Order { get; set; }
+        public readonly List<string> Guest {  get; set; }
+        public string Resume { get; set; }
+        public string Title { get; set; }
+
+
+        public void AddGuest(string guest)
+        {
+            Guest.Add(guest);
+        }
+    }
+}
